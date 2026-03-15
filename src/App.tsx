d8b7059b23d8ebe23b5859d6d3ddfe3f5967c762
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-03-15 11:38:18 +0800

    FOOTER - HEADER

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
-import Footer from './components/Footer';
+import Footer from './components/Header';
 
 const App: React.FC = () => {
   return (

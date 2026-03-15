0a26db158ec83163797c4e1170d24aaaa06ca047
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-03-15 18:09:03 +0800

    additional skill

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     const categories = [
         {
             title: 'Web Development',
-            skills: ['React', 'Next.js', 'Node.js', 'Supabase', 'Tailwind CSS']
+            skills: ['React', 'Next.js', 'Node.js', 'Supabase', 'Tailwind CSS', 'Html', 'Css', 'Javascript']
         },
         {
             title: 'Game Development',

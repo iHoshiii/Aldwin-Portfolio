2cb132166a42207dbaddd5204a567c31048723db
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-03-15 16:17:18 +0800

    test2

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -47,7 +47,7 @@ export const projects: Project[] = [
         githubLink: "https://www.facebook.com/share/v/1F1STNPZoL/",
         tech: ["Unity", "Unity Version Control", "C#", "Photoshop", "Blender"],
         primaryLinkLabel: "Game Video",
-        secondaryLinkLabel: "Documentations",
+        secondaryLinkLabel: "Documentation",
         tertiaryLink: "/PacMadFiles/PacMadPaper.docx",
         tertiaryLinkLabel: "Game Paper",
         gallery: [

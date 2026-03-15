5480c9180021eb5c441bcb6537cd41dc6a017f10
Aldwin Loreto <aldwinloreto690@gmail.com>
2026-03-15 12:28:36 +0800

    repository link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,21 @@ const Navbar: React.FC = () => {
                     Aldwin Loreto<span className="text-black/40">'s Portfolio</span>
                 </div>
                 <ul className="hidden md:flex gap-4">
-                    {['Home', 'Background', 'Projects', 'Contacts'].map((item) => (
-                        <li key={item}>
+                    {[
+                        { name: 'Home', href: '#home' },
+                        { name: 'Background', href: '#background' },
+                        { name: 'Projects', href: '#projects' },
+                        { name: 'Contacts', href: '#contacts' },
+                        { name: 'Repository', href: 'https://github.com/iHoshiii?tab=repositories', external: true }
+                    ].map((item) => (
+                        <li key={item.name}>
                             <a
-                                href={`#${item.toLowerCase()}`}
+                                href={item.href}
+                                target={item.external ? "_blank" : undefined}
+                                rel={item.external ? "noopener noreferrer" : undefined}
                                 className="text-sm font-bold text-black border-2 border-black px-4 py-2 rounded-full hover:bg-black hover:text-white transition-all duration-300"
                             >
-                                {item}
+                                {item.name}
                             </a>
                         </li>
                     ))}
